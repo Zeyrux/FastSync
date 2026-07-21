7d14d2b672eaa881534f9c7837b28498a5e48437
TapTap <taptap.programming@gmail.com>
2026-07-21 17:45:26 +0200

    fix: const qualifier for scanner root_directory parameter

--- src/client/scanner.h
+++ src/client/scanner.h
@@ -22,7 +22,7 @@ typedef struct {
   int current_depth;
 } DirectoryScanner;
 
-DirectoryScanner* directory_scanner_create(char* root_directory, bool use_metadata,
+DirectoryScanner* directory_scanner_create(const char* root_directory, bool use_metadata,
                                            unsigned long long chunk_size, char** exclude_patterns,
                                            int exclude_count, char** include_patterns,
                                            int include_count, unsigned long long max_size,
