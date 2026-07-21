7436b3b148bb730cc0395abd33bc3ae237882772
TapTap <taptap.programming@gmail.com>
2026-07-21 18:07:03 +0200

    chore: add build2/ to gitignore and remove from tracking

--- build2/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/FastSyncProjects/FastSyncBuilder3
-# Build directory: /root/FastSyncProjects/FastSyncBuilder3/build2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[unit_all]=] "/root/FastSyncProjects/FastSyncBuilder3/build2/tests")
-set_tests_properties([=[unit_all]=] PROPERTIES  _BACKTRACE_TRIPLES "/root/FastSyncProjects/FastSyncBuilder3/CMakeLists.txt;85;add_test;/root/FastSyncProjects/FastSyncBuilder3/CMakeLists.txt;0;")
-subdirs("_deps/xxhash-build")
--- build2/_deps/xxhash-build/CMakeFiles/xxhsum.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for xxhsum.
-# This may be replaced when dependencies are built.
