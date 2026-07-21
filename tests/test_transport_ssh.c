d21f6c8be9c30a550168fae25d204e19033cc6bb
TapTap <taptap.programming@gmail.com>
2026-07-21 18:10:41 +0200

    fix: cppcheck suppress constVariablePointer in test

--- tests/test_transport_ssh.c
+++ tests/test_transport_ssh.c
@@ -3,11 +3,13 @@
 #include "transport_ssh.h"
 
 static void test_ssh_connect_invalid_dest_no_colon() {
+  /* cppcheck-suppress constVariablePointer */
   Client* c = client_connect_ssh("/path/to/dest", 22);
   EXPECT_NULL(c);
 }
 
 static void test_ssh_connect_invalid_dest_empty() {
+  /* cppcheck-suppress constVariablePointer */
   Client* c = client_connect_ssh("", 22);
   EXPECT_NULL(c);
 }
