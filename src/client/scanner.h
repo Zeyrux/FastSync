e68e33c0c5afd19b163bc331e8e062062b1e5db0
TapTap <taptap.programming@gmail.com>
2026-07-29 18:18:07 +0200

    Apply PR #143 content on top of latest main

--- src/client/scanner.h
+++ src/client/scanner.h
@@ -5,6 +5,7 @@
 #include "queue.h"
 #include <dirent.h>
 #include <stdbool.h>
+#include <threads.h>
 
 typedef struct {
   Queue* directories;
@@ -18,21 +19,37 @@ typedef struct {
   int include_count;
   unsigned long long max_size;
   unsigned long long min_size;
-  bool follow_symlinks;
+  int max_depth;
+  int current_depth;
 } DirectoryScanner;
 
-DirectoryScanner* directory_scanner_create(char* root_directory, bool use_metadata,
+typedef struct {
+  Queue* result_queue;
+  mtx_t result_mutex;
+  cnd_t result_not_empty;
+  cnd_t result_not_full;
+  int num_threads;
+  thrd_t* threads;
+  bool done;
+  int completed;
+  Chunk* initial_chunk;
+} ParallelScanner;
+
+DirectoryScanner* directory_scanner_create(const char* root_directory, bool use_metadata,
                                            unsigned long long chunk_size, char** exclude_patterns,
                                            int exclude_count, char** include_patterns,
                                            int include_count, unsigned long long max_size,
-                                           unsigned long long min_size);
-DirectoryScanner* directory_scanner_create_full(char* root_directory, bool use_metadata,
-                                                unsigned long long chunk_size,
-                                                char** exclude_patterns, int exclude_count,
-                                                char** include_patterns, int include_count,
-                                                unsigned long long max_size,
-                                                unsigned long long min_size, bool follow_symlinks);
+                                           unsigned long long min_size, int max_depth);
 Chunk* directory_scanner_next(DirectoryScanner* scanner);
 void directory_scanner_destroy(DirectoryScanner* scanner);
+
+ParallelScanner* parallel_scanner_create(char* root_directory, bool use_metadata,
+                                         unsigned long long chunk_size, char** exclude_patterns,
+                                         int exclude_count, char** include_patterns,
+                                         int include_count, unsigned long long max_size,
+                                         unsigned long long min_size, int max_depth,
+                                         int num_threads);
+Chunk* parallel_scanner_next(ParallelScanner* scanner);
+void parallel_scanner_destroy(ParallelScanner* scanner);
 
 #endif
